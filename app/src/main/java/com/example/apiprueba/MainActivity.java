870b22abde1cd06245708faf5145488aa0695911
Jose_Maria <josemariazapataq2@gmail.com>
2023-10-07 15:29:13 -0400

    hackaton

--- app/src/main/java/com/example/apiprueba/MainActivity.java
+++ app/src/main/java/com/example/apiprueba/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.apiprueba;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -25,19 +27,21 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.opencsv.CSVReader;
 
 import org.json.JSONObject;
 
 import java.io.StringReader;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     Button boton;
     GoogleMap mapView;
-    String api = "https://firms.modaps.eosdis.nasa.gov/api/country/csv/95228f5172a6476487cea9ca7348bf58/MODIS_NRT/BOL/1/2023-10-05/";
+    String api = "https://firms.modaps.eosdis.nasa.gov/api/country/csv/692cc45f56bcc891b36d5be7599d8f5c/MODIS_NRT/BOL/1/2023-10-05/";
     List<Coordinate> coordinatesList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,18 +56,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Hola mundo", Toast.LENGTH_SHORT).show();
 
-
+                Marker ubicacion = null; // Cambiado de Circle a Marker
                 for (Coordinate coordinates : coordinatesList) {
-                    Circle ubicacion = mapView.addCircle(new CircleOptions()
-                            .center(new LatLng(coordinates.latitude,coordinates.longitude))
-                            .radius(100)
-                            .strokeColor(Color.argb(128, 255, 0, 0))
-                            .fillColor(Color.argb(128, 255, 0, 0)));
-                    mapView.moveCamera(CameraUpdateFactory.newLatLngZoom(ubicacion.getCenter(), 15));
+                    ubicacion = mapView.addMarker(new MarkerOptions()
+                            .position(new LatLng(coordinates.latitude, coordinates.longitude)));
                     Log.d("coordenadas", coordinates.toString());
+                }
+                if (ubicacion != null) {
+                    mapView.moveCamera(CameraUpdateFactory.newLatLngZoom(ubicacion.getPosition(), 15));
                 }
             }
         });
+
 
     }
 
@@ -76,20 +80,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             coordinatesList.clear();
             String[] headers = data.get(0);
 
-            //int latitudeIndex = -1;
-            //int longitudeIndex = -1;
 
-            //for (int i = 0; i < headers.length; i++) {
-            //    if ("latitude".equals(headers[i])) {
-            //        latitudeIndex = i;
-            //    } else if ("longitude".equals(headers[i])) {
-            //        longitudeIndex = i;
-            //    }
-            //}
             for (int i = 1; i < data.size(); i++) {
                 String[] row = data.get(i);
-                String latitude = row[1];//"\"" + row[1] + "\"";
-                String longitude = row[2]; //= "\"" + row[longitudeIndex] + "\"";
+                String latitude = row[1];
+                String longitude = row[2];
                 coordinatesList.add(new Coordinate("BOL",
                         Double.parseDouble(latitude),
                         Double.parseDouble(longitude)));
@@ -128,5 +123,34 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mapView = googleMap;
+
+        mapView.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                for (Coordinate coordinates : coordinatesList) {
+                    if (marker.getPosition().latitude == coordinates.latitude &&
+                            marker.getPosition().longitude == coordinates.longitude) {
+                        openWhatsApp("+59168830275", coordinates.latitude, coordinates.longitude);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
     }
+
+    private void openWhatsApp(String phoneNumber, double latitude, double longitude) {
+        String mapUrl = "https://www.google.com/maps/place/" + latitude + "," + longitude + " A fire has been reported at this location HELP NEEDED";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        String url = "https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + URLEncoder.encode(mapUrl);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
 }
+
+
+
+
+
